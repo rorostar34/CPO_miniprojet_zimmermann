0a3924ec7ba0648726ccdf71d325a87edc6291f8
Romane Zimmermann <zimromane@gmail.com>
2024-12-02 15:06:05 +0100

    miniprojet démineur projet commence a relie grille et console graphique s'affiche modif de cellule graph

--- CPO_miniprojet_Zimmermann_version_graphique/src/cpo_miniprojet_zimmermann/Cellule_Graphique.java
+++ CPO_miniprojet_Zimmermann_version_graphique/src/cpo_miniprojet_zimmermann/Cellule_Graphique.java
@@ -2,14 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cpo_miniprojet_zimmermann;
-
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- *
- * @author 33669
- */
 public class Cellule_Graphique extends JButton {
     int x;
     int y;
@@ -19,6 +15,31 @@ public class Cellule_Graphique extends JButton {
         this.x = x;
         this.y = y;
         this.cellule_associe = cellule_associe;
+
+        // Ajouter un ActionListener pour gérer le clic sur ce bouton
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Révéler la cellule associée
+                if (!cellule_associe.estDevoilee()) {
+                    cellule_associe.revelerCellule();
+                    mettreAJourAffichage();
+                }
+            }
+        });
+    }
+
+    // Méthode pour mettre à jour l'affichage du bouton
+    public void mettreAJourAffichage() {
+        if (cellule_associe.estDevoilee()) {
+            if (cellule_associe.getPresenceBombe()) {
+                setText("B"); // Afficher une bombe
+            } else if (cellule_associe.getNbBombesAdjacentes() > 0) {
+                setText(String.valueOf(cellule_associe.getNbBombesAdjacentes())); // Afficher le nombre de bombes adjacentes
+            } else {
+                setText(""); // Cellule vide
+            }
+            setEnabled(false); // Désactiver le bouton une fois dévoilé
+        }
     }
 }
-    
